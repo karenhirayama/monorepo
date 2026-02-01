dc3e6b7476a42b221781f5cfd80a8142dfdefaa1
karenhirayama <karenm.hirayama@gmail.com>
2026-02-01 15:25:42 -0300

    feat: Add reset form functionality

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -1,4 +1,3 @@
-// useForm.js
 import { useState } from 'react';
 
 const useForm = (initialState) => {
@@ -8,7 +7,11 @@ const useForm = (initialState) => {
     setFormData({ ...formData, [field]: value });
   };
 
-  return { formData, handleChange };
+  const resetForm = () => {
+    setFormData(initialState);
+  };
+
+  return { formData, handleChange, resetForm };
 };
 
 export default useForm;
