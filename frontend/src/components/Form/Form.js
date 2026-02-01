e2b7083a93846fcfbb8362f38173e660fd49956c
karenhirayama <karenm.hirayama@gmail.com>
2026-02-01 15:23:11 -0300

    feat: Update logic in Form to handle change recommendations

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,13 +1,11 @@
-// Form.js
-
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Preferences, Features, RecommendationType } from './Fields';
 import { SubmitButton } from './SubmitButton';
 import useProducts from '../../hooks/useProducts';
 import useForm from '../../hooks/useForm';
 import useRecommendations from '../../hooks/useRecommendations';
 
-function Form() {
+function Form({ onRecommendationsChange }) {
   const { preferences, features, products } = useProducts();
   const { formData, handleChange } = useForm({
     selectedPreferences: [],
@@ -15,22 +13,19 @@ function Form() {
     selectedRecommendationType: '',
   });
 
-  const { getRecommendations, recommendations } = useRecommendations(products);
+  const { getRecommendations } = useRecommendations(products);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const dataRecommendations = getRecommendations(formData);
 
-    /**
-     * Defina aqui a lógica para atualizar as recomendações e passar para a lista de recomendações
-     */
+    if (onRecommendationsChange) {
+      onRecommendationsChange(dataRecommendations);
+    }
   };
 
   return (
-    <form
-      className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md"
-      onSubmit={handleSubmit}
-    >
+    <form onSubmit={handleSubmit} className="space-y-8">
       <Preferences
         preferences={preferences}
         onPreferenceChange={(selected) =>
@@ -47,8 +42,9 @@ function Form() {
         onRecommendationTypeChange={(selected) =>
           handleChange('selectedRecommendationType', selected)
         }
+        selectedType={formData.selectedRecommendationType}
       />
-      <SubmitButton text="Obter recomendação" />
+      <SubmitButton text="Gerar Recomendações" />
     </form>
   );
 }
